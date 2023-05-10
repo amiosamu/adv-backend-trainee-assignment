81311f98620c617f58bcdd4b8af118881b308428
amiosamu <damitullaev@gmail.com>
2023-05-10 16:45:39 +0600

    Starting over again

--- advertising/schema/000001_init.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TABLE advertising
-(
-    uuid uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-    name VARCHAR NOT NULL DEFAULT '',
-    description VARCHAR NOT NULL DEFAULT '',
-    links TEXT[] NOT NULL,
-    price NUMERIC NOT NULL
-)
