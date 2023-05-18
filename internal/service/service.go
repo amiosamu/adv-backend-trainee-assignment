3471d56178a7e30d05221aa0b575d5797aa01cb7
amiosamu <damitullaev@gmail.com>
2023-05-18 15:47:57 +0600

    update docker and docker-compose.yml. minor fixes

--- internal/service/service.go
+++ internal/service/service.go
@@ -6,6 +6,24 @@ import (
 	"github.com/amiosamu/adv-backend-trainee-assignment/internal/repo"
 )
 
+type Dependencies struct {
+	Repos *repo.Repos
+}
+
+type AdvertisementInput struct {
+	Id       int
+	SortType string
+	Offset   int
+	Limit    int
+}
+type AdvertisementOutput struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Pictures    string `json:"pictures"`
+	Price       int    `json:"price"`
+	Order       *int   `json:"order"`
+}
+
 type Advertisement interface {
 	CreateAdvertisement(ctx context.Context, entity entity.Advertisement) (int, error)
 	GetAdvertisementById(ctx context.Context, id int) (entity.Advertisement, error)
@@ -14,10 +32,6 @@ type Advertisement interface {
 
 type Services struct {
 	Advertisement Advertisement
-}
-
-type Dependencies struct {
-	Repos *repo.Repos
 }
 
 func NewServices(deps Dependencies) *Services {
