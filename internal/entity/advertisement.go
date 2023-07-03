868c5a99b0bf13b3212f3ea2bd57a44961b63945
amiosamu <damitullaev@gmail.com>
2023-07-03 17:38:25 +0600

    Update entity

--- internal/entity/advertisement.go
+++ internal/entity/advertisement.go
@@ -4,9 +4,9 @@ import "time"
 
 type Advertisement struct {
 	Id          int       `db:"id"`
-	Name        string    `db:"name"`
+	Name        string    `db:"name" `
 	Description string    `db:"description"`
-	Pictures    []string  `db:"pictures"`
+	Pictures    string    `db:"pictures"`
 	Price       int       `db:"price"`
 	CreatedAt   time.Time `db:"created_at"`
 }
