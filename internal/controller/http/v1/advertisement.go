119c4283fe8e10a229f4cc2053bed1e97318108a
amiosamu <damitullaev@gmail.com>
2023-07-03 17:39:11 +0600

    Rewriting handlers

--- internal/controller/http/v1/advertisement.go
+++ internal/controller/http/v1/advertisement.go
@@ -1,11 +1,9 @@
 package v1
 
 import (
-	"github.com/amiosamu/adv-backend-trainee-assignment/internal/entity"
 	"github.com/amiosamu/adv-backend-trainee-assignment/internal/service"
 
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 type advertisementRoutes struct {
@@ -33,15 +31,7 @@ func newAdvertisementRoutes(c *gin.RouterGroup, advertisementService service.Adv
 // @Router /api/v1/advertisements/create [post]
 
 func (r *advertisementRoutes) create(context *gin.Context) {
-	var input entity.Advertisement
-	id, err := r.advertisementService.CreateAdvertisement(context.Request.Context(), input)
-	if err != nil {
-		newErrorResponse(context, http.StatusBadRequest, err.Error())
-		return
-	}
-	context.JSON(http.StatusCreated, map[string]interface{}{
-		"id": id,
-	})
+
 }
 
 type getByIdInput struct {
@@ -59,16 +49,5 @@ type getByIdInput struct {
 // @Failure default {object} errorResponse
 // @Router /api/v1/advertisements/:id [post]
 func (r *advertisementRoutes) get(context *gin.Context) {
-	var input getByIdInput
 
-	if err := context.BindJSON(input); err != nil {
-		newErrorResponse(context, http.StatusBadRequest, err.Error())
-		return
-	}
-	advertisement, err := r.advertisementService.GetAdvertisementById(context.Request.Context(), input.Id)
-	if err != nil {
-		newErrorResponse(context, http.StatusBadRequest, err.Error())
-		return
-	}
-	context.JSON(http.StatusOK, advertisement)
 }
